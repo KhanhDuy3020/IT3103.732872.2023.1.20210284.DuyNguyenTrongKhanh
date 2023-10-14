5ec32c71c55c41ce39de2cc8aa99ec76b762706b
KhanhDuy3020 <khanhduyak30.tqp@gmai.com>
2023-10-14 18:02:16 +0700

    Lab01_1

---
+++ JavaBasics/src/Lab_01/Calculate.java
@@ -0,0 +1,32 @@
+// Example 5: Calculate.java
+import javax.swing.JOptionPane;
+
+public class Calculate {
+    public static void main(String[] args) {
+        double num1, num2, sum, difference, product, quotient;
+        String strNotification = "";
+
+        num1 = Double.parseDouble(JOptionPane.showInputDialog(null, 
+                "Nguyen Trong Khanh Duy 20210284 - Please input the first number: ",
+                "Input the first number",
+                JOptionPane.INFORMATION_MESSAGE));
+
+        num2 = Double.parseDouble(JOptionPane.showInputDialog(null, 
+                "Nguyen Trong Khanh Duy 20210284 - Please input the second number: ",
+                "Input the second number",
+                JOptionPane.INFORMATION_MESSAGE));
+
+        sum = num1 + num2; // Tính tổng 2 số
+        difference = num1 - num2; // Tính hiệu 2 số
+        product = num1 * num2; // Tính tích 2 số
+        quotient = num1 / num2; // Tính thương 2 số
+
+        strNotification += "Sum = " + sum + "\n";
+        strNotification += "Difference = " + difference + "\n";
+        strNotification += "Product = " + product + "\n";
+        strNotification += "Quotient = " + quotient + "\n";
+
+        JOptionPane.showMessageDialog(null, strNotification, "Show calculate", JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+    }
+}
---
+++ JavaBasics/src/Lab_01/ShowTwoNumbers.java
@@ -0,0 +1,25 @@
+// Example 5: ShowTwoNumbers.java
+import javax.swing.JOptionPane;
+
+public class ShowTwoNumbers {
+
+    public static void main(String[] args) {
+        String strNuml, strNum2;
+        String strNotification = "You've just entered: ";
+        strNuml = JOptionPane.showInputDialog(null, 
+                "Nguyen Trong Khanh Duy 20210284 - Please input the first number: ",
+                "Input the first number",
+                JOptionPane.INFORMATION_MESSAGE);
+        strNotification += strNuml + " and ";
+
+        strNum2 = JOptionPane.showInputDialog(null, 
+                "Nguyen Trong Khanh Duy 20210284 - Please input the second number: ",
+                "Input the second number",
+                JOptionPane.INFORMATION_MESSAGE);
+
+        strNotification += strNum2;
+
+        JOptionPane.showMessageDialog(null, strNotification, "Show two numbers", JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+    }
+}
